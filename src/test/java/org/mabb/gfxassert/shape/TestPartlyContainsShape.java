6fff77f0db5131ebbf05163361ec29a9adf97773
meese <matt.w.abboud@gmail.com>
2016-06-05 15:01:58 -0600

    add LGPL header to all files

--- src/test/java/org/mabb/gfxassert/shape/TestPartlyContainsShape.java
+++ src/test/java/org/mabb/gfxassert/shape/TestPartlyContainsShape.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) Matthew Abboud 2016
+ *
+ * GfxAssert is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GfxAssert is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GfxAssert. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.mabb.gfxassert.shape;
 
 import org.junit.Assert;
@@ -30,7 +47,7 @@ public class TestPartlyContainsShape {
         Rectangle2D.Double targetRect = new Rectangle2D.Double(0, 0, 340, 355);
         Assert.assertThat(containingRect, partiallyContains(targetRect));
     }
-    
+
     @Test
     public void givenRectNotInContainer_whenAssertPartlyInContainer_thenFalse() {
         Rectangle2D.Double targetRect = new Rectangle2D.Double(400, 400, 5, 5);
